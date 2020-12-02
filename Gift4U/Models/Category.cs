b39bfa17c64bc69611ff9fb3f05c68c13cadf60f
chenittah <chenpachima@gmail.com>
2020-12-02 22:29:33 +0200

    add Required

--- Gift4U/Models/Category.cs
+++ Gift4U/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Gift4U.Models
     public class Category
     {
         public int Id { get; set; }
+
+        [Required]
         public String Name { get; set; }
         public int Type { get; set; }
         public ICollection<SubCategory> SubCategories { get; set; }
