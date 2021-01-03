28f7c7a1c5510aadb3777e6c976aef36388beab7
chenittah <chenpachima@gmail.com>
2021-01-03 02:40:15 +0200

    add category controller' create db, add photos files

---
+++ Gift4U/Migrations/20210102233042_usercntrlr.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gift4U.Migrations
+{
+    public partial class usercntrlr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
